3c3098fade564cf6e8fba21a83d8efed102abde4
David G. Simmons <santafen@mac.com>
2016-02-03 17:16:30 -0500

    New Startup script for /etc/init.d
    Auto-start MongoDB and the Strongloop Application

---
+++ demo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+systemctl start strong-pm
+rm -rf /data/db/edison*
+/usr/local/bin/mongod --storageEngine=mmapv1 2>$1 > /home/root/mongo.log &
+sleep 5
+node /home/root/StrongLoop-IoT-Demo &
