6a19a0a6633ecb3343449b99e9114f2df759eaf8
David G. Simmons <santafen@mac.com>
2016-02-05 14:05:13 -0500

    Fix data lock

--- demo.sh
+++ demo.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 systemctl start strong-pm
 rm -rf /data/db/edison*
+rm -rf /data/db/*lock*
 /usr/local/bin/mongod --storageEngine=mmapv1 > /home/root/mongo.log &
 sleep 5
 node /home/root/StrongLoop-IoT-Demo &
