7215d3f0ab74bbc7d3aad6dfece4380b50d71c9f
David G. Simmons <santafen@mac.com>
2016-02-05 11:26:25 -0500

    resart sensor logger from web

---
+++ resetSensors.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+pid=`ps | grep sensors | grep -v grep | awk '{print $1}'`
+if [ $pid ]; then
+   kill $pid
+fi
+echo "restarating Sensors ... "
+cd /home/root/LSM9DS0
+./sensors --output json --dbhost localhost &
